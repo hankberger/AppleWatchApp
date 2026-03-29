aa23e36239460cd3ad47d62a2dd612058c1c8fc5
hankberger <hanksberger@gmail.com>
2026-03-29 12:18:35 -0500

    Initial Commit

---
+++ FirstWatchApp Watch App/FirstWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstWatchAppApp.swift
+//  FirstWatchApp Watch App
+//
+//  Created by Hank Berger on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstWatchApp Watch AppTests/FirstWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstWatchApp_Watch_AppTests.swift
+//  FirstWatchApp Watch AppTests
+//
+//  Created by Hank Berger on 3/29/26.
+//
+
+import Testing
+@testable import FirstWatchApp_Watch_App
+
+struct FirstWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
